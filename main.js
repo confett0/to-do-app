86c2a48cb0e66f782735f80e69437f3622c3a498
confett0 <118047961+confett0@users.noreply.github.com>
2023-03-03 16:31:41 +0100

    Refactor code to use projects as task properties instead of objects

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/dom.js":
@@ -16,7 +15,8 @@
   \********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createProjectList\": () => (/* binding */ createProjectList),\n/* harmony export */   \"displayTodos\": () => (/* binding */ displayTodos)\n/* harmony export */ });\n/* harmony import */ var _task__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task */ \"./src/task.js\");\n/* harmony import */ var _projects__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./projects */ \"./src/projects.js\");\n\n\n\nconst wrap = document.querySelector(\".todo-wrap\");\nconst projectWrap = document.querySelector(\".project-list\");\n\nconst createElement = (el, className, content) => {\n  const element = document.createElement(el);\n  element.setAttribute(\"class\", className);\n  element.textContent = content;\n  return element;\n};\n\nconst createTodoDiv = (todo, project) => {\n  const todoDiv = createElement(\"div\", \"todo\");\n  todoDiv.id = project.list.indexOf(todo);\n\n  const checkbox = createElement(\"input\", \"checkbox\");\n  checkbox.setAttribute(\"type\", \"checkbox\");\n\n  const todoName = createElement(\"p\", \"todo-name\", todo.name);\n\n  const deleteTodo = createElement(\"div\", \"delete-button\", \"x\");\n\n  todoDiv.append(checkbox, todoName, deleteTodo);\n  wrap.appendChild(todoDiv);\n\n  // move event listeners somewhere for cleaner code\n  checkbox.addEventListener(\"change\", todo.doneUndone);\n  deleteTodo.addEventListener(\"click\", () => {\n    project.deleteTask(todo);\n    displayTodos(project);\n  });\n\n  return todoDiv;\n};\n\nconst displayTodos = (project) => {\n  wrap.innerHTML = \"\";\n  project.list.map((todo) => createTodoDiv(todo, project));\n};\n\n// Sidebar\n\nconst inboxLink = document.getElementById(\"inbox\");\ninboxLink.addEventListener(\"click\", () => {\ndisplayTodos(_projects__WEBPACK_IMPORTED_MODULE_1__.inbox);\n});\n\nconst createProjectLi = (project) => {\n  const projectLi = createElement(\"li\", \"project\", project.name);\n  const idName = project.name.toLowerCase()\n  projectLi.setAttribute(\"id\", idName);\n\n  projectWrap.appendChild(projectLi);\n\n  projectLi.addEventListener(\"click\", () => displayTodos(project));\n\n};\n\nconst createProjectList = () => {\n  projectWrap.innerHTML = \"\";\n  _projects__WEBPACK_IMPORTED_MODULE_1__.projectList.list.map((project) => createProjectLi(project));\n\n};\n\n// Todo form\n\nconst todoForm = document.getElementById(\"new-todo\");\n\ntodoForm.onsubmit = (e) => {\n  e.preventDefault();\n  const formData = new FormData(todoForm);\n  const newTodo = {};\n\n  formData.forEach((value, key) => (newTodo[key] = value));\n\n  _projects__WEBPACK_IMPORTED_MODULE_1__.inbox.addTask(newTodo.name, newTodo.category, newTodo.priority);\n\n  todoForm.reset();\n  displayTodos(_projects__WEBPACK_IMPORTED_MODULE_1__.inbox);\n};\n\n// Project form\n\nconst projectForm = document.getElementById(\"project-form\");\n\nprojectForm.onsubmit = (e) => {\n  e.preventDefault();\n  const formData = new FormData(projectForm);\n  const newProject = {};\n\n  formData.forEach((value, key) => (newProject[key] = value));\n  _projects__WEBPACK_IMPORTED_MODULE_1__.projectList.createProject(newProject.name);\n  createProjectList();\n  projectForm.reset();\n};\n\n\n\n\n\n//# sourceURL=webpack://to-do-app/./src/dom.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createProjectList\": () => (/* binding */ createProjectList),\n/* harmony export */   \"displayTodos\": () => (/* binding */ displayTodos)\n/* harmony export */ });\n/* harmony import */ var _task__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task */ \"./src/task.js\");\n/* harmony import */ var _projects__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./projects */ \"./src/projects.js\");\n/* harmony import */ var _projects__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_projects__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\nconst wrap = document.querySelector(\".todo-wrap\");\nconst projectWrap = document.querySelector(\".project-list\");\n\nconst createElement = (el, className, content) => {\n  const element = document.createElement(el);\n  element.setAttribute(\"class\", className);\n  element.textContent = content;\n  return element;\n};\n\nconst createTodoDiv = (todo) => {\n  const todoDiv = createElement(\"div\", \"todo\");\n  todoDiv.id = _task__WEBPACK_IMPORTED_MODULE_0__.taskManager.list.indexOf(todo);\n\n  const checkbox = createElement(\"input\", \"checkbox\");\n  checkbox.setAttribute(\"type\", \"checkbox\");\n\n  const todoName = createElement(\"p\", \"todo-name\", todo.name);\n\n  const deleteTodo = createElement(\"div\", \"delete-button\", \"x\");\n\n  todoDiv.append(checkbox, todoName, deleteTodo);\n  wrap.appendChild(todoDiv);\n\n  // move event listeners somewhere for cleaner code\n  checkbox.addEventListener(\"change\", todo.doneUndone);\n  deleteTodo.addEventListener(\"click\", () => {\n    _task__WEBPACK_IMPORTED_MODULE_0__.taskManager.deleteTask(todo);\n    displayTodos();\n  });\n\n  return todoDiv;\n};\n\nconst displayTodos = () => {\n  wrap.innerHTML = \"\";\n  _task__WEBPACK_IMPORTED_MODULE_0__.taskManager.list.map((todo) => createTodoDiv(todo));\n};\n\n// Sidebar\n\nconst showAll = document.getElementById(\"show-all\");\nshowAll.addEventListener(\"click\", () => {\n  displayTodos();\n});\n\nconst createProjectLi = (project) => {\n  const projectLi = createElement(\"li\", \"project\", project);\n  const idName = project.toLowerCase();\n  projectLi.setAttribute(\"id\", idName);\n\n  projectWrap.appendChild(projectLi);\n\n  // projectLi.addEventListener(\"click\", () => displayTodos(project));\n};\n\nconst createProjectList = () => {\n  projectWrap.innerHTML = \"\";\n  _task__WEBPACK_IMPORTED_MODULE_0__.taskManager.categories.map((project) => createProjectLi(project));\n};\n\n// Todo form\n\nconst todoForm = document.getElementById(\"new-todo\");\n\ntodoForm.onsubmit = (e) => {\n  e.preventDefault();\n  const formData = new FormData(todoForm);\n  const newTodo = {};\n\n  formData.forEach((value, key) => (newTodo[key] = value));\n\n  _task__WEBPACK_IMPORTED_MODULE_0__.taskManager.addTask(newTodo.name);\n  displayTodos();\n  todoForm.reset();\n};\n\n// Project form\n\nconst projectForm = document.getElementById(\"project-form\");\n\nprojectForm.onsubmit = (e) => {\n  e.preventDefault();\n  const formData = new FormData(projectForm);\n  const newProject = {};\n\n  formData.forEach((value, key) => (newProject[key] = value));\n  _task__WEBPACK_IMPORTED_MODULE_0__.taskManager.categories.push(newProject.name);\n  createProjectList();\n  projectForm.reset();\n};\n\n\n\n\n//# sourceURL=webpack://to-do-app/./src/dom.js?");
 
 /***/ }),
 
@@ -26,7 +26,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dom */ \"./src/dom.js\");\n/* harmony import */ var _projects__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./projects */ \"./src/projects.js\");\n\n\n\n(0,_dom__WEBPACK_IMPORTED_MODULE_0__.displayTodos)(_projects__WEBPACK_IMPORTED_MODULE_1__.inbox);\n(0,_dom__WEBPACK_IMPORTED_MODULE_0__.createProjectList)();\n\n//# sourceURL=webpack://to-do-app/./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dom */ \"./src/dom.js\");\n/* harmony import */ var _projects__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./projects */ \"./src/projects.js\");\n/* harmony import */ var _projects__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_projects__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\n(0,_dom__WEBPACK_IMPORTED_MODULE_0__.displayTodos)();\n(0,_dom__WEBPACK_IMPORTED_MODULE_0__.createProjectList)();\n\n//# sourceURL=webpack://to-do-app/./src/index.js?");
 
 /***/ }),
 
@@ -34,9 +35,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dom
 /*!*************************!*\
   !*** ./src/projects.js ***!
   \*************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Project\": () => (/* binding */ Project),\n/* harmony export */   \"inbox\": () => (/* binding */ inbox),\n/* harmony export */   \"projectList\": () => (/* binding */ projectList),\n/* harmony export */   \"work\": () => (/* binding */ work)\n/* harmony export */ });\n/* harmony import */ var _task__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task */ \"./src/task.js\");\n\n\nclass Project {\n    constructor(name) {\n        this.name = name;\n        this.list = [];\n    }\n\n    addTask(taskName, taskCategory, taskDate, taskPriority) {\n        this.list.push(new _task__WEBPACK_IMPORTED_MODULE_0__.Task(taskName, taskCategory, taskDate, taskPriority));\n      }\n\n    deleteTask(task) {\n        this.list.splice(this.list.indexOf(task),1);\n    }\n}\n\nconst projectList = {\n\n    list: [],\n\n    createProject(name) {\n    const newProject = new Project(name);\n    this.list.push(newProject);\n        return newProject;\n    },\n\n    deleteProject(name) {\n        this.list.splice(this.list.indexOf(name,1));\n    }\n}\n\nconst inbox = new Project(\"Inbox\");\nconst work = projectList.createProject(\"Work\");\nwork.addTask(\"code\");\n\n\ninbox.addTask(\"eat\");\ninbox.addTask(\"sleep\");\ninbox.addTask(\"repeat\");\n\n\n\n//# sourceURL=webpack://to-do-app/./src/projects.js?");
+eval("\n\n//# sourceURL=webpack://to-do-app/./src/projects.js?");
 
 /***/ }),
 
@@ -46,7 +47,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Task\": () => (/* binding */ Task)\n/* harmony export */ });\nclass Task {\n  constructor(name, category, priority) {\n    this.name = name;\n    this.category = category;\n    // this.dueDate = dueDate; we'll had date later\n    this.priority = priority;\n    this.done = false;\n  }\n\n  doneUndone() {\n    if (this.done === false) {\n      this.done = true;\n    } else {\n      this.done = false;\n    }\n    console.log(this);\n  }\n}\n\n\n\n\n//# sourceURL=webpack://to-do-app/./src/task.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Task\": () => (/* binding */ Task),\n/* harmony export */   \"taskManager\": () => (/* binding */ taskManager)\n/* harmony export */ });\nclass Task {\n  constructor(name, category, priority) {\n    this.name = name;\n    this.category = category;\n    // this.dueDate = dueDate; we'll had date later\n    this.priority = priority;\n    this.done = false;\n  }\n\n  doneUndone() {\n    if (this.done === false) {\n      this.done = true;\n    } else {\n      this.done = false;\n    }\n  }\n}\n\nconst taskManager = {\n  list: [],\n  categories: [],\n\n  addTask(taskName, taskCategory, taskDate, taskPriority) {\n    this.list.push(new Task(taskName, taskCategory, taskDate, taskPriority));\n  },\n\n  deleteTask(task) {\n    this.list.splice(this.list.indexOf(task), 1);\n  },\n};\n\ntaskManager.addTask(\"eat\");\ntaskManager.addTask(\"sleep\");\ntaskManager.addTask(\"repeat\");\n\n\n\n\n//# sourceURL=webpack://to-do-app/./src/task.js?");
 
 /***/ })
 
@@ -77,6 +79,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
